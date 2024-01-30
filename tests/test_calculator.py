15b2f777e8f686bc51005076f495a8d25900033d
Pra5etya <careermagic780@gmail.com>
2024-01-30 15:58:35 +0700

    py modification and add note

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -4,11 +4,11 @@
 from codes.calculator import add, subtract
 
 def test_add():
-    assert add(2, 3) == 5
-    assert add(-1, 1) == 0
-    assert add(-1, -1) == -2
+    assert add(2, 3) == 5, 'The input results do not match the output'
+    assert add(-1, 1) == 0, 'The input results do not match the output'
+    assert add(-1, -1) == -2, 'The input results do not match the output'
 
 def test_subtract():
-    assert subtract(5, 3) == 2
-    assert subtract(-1, 1) == -2
-    assert subtract(-1, -1) == 0
+    assert subtract(5, 3) == 2, 'The input results do not match the output'
+    assert subtract(-1, 1) == -2, 'The input results do not match the output'
+    assert subtract(-1, -1) == 0, 'The input results do not match the output'
--- tests/test_math_operations.py
+++ tests/test_math_operations.py
@@ -4,14 +4,14 @@
 from codes.math_operations import multiply, divide
 
 def test_multiply():
-    assert multiply(2, 3) == 6
-    assert multiply(-1, 1) == -1
-    assert multiply(-1, -1) == 1
+    assert multiply(2, 3) == 6, 'The input results do not match the output'
+    assert multiply(-1, 1) == -1, 'The input results do not match the output'
+    assert multiply(-1, -1) == 1, 'The input results do not match the output'
 
 def test_divide():
-    assert divide(6, 3) == 2
-    assert divide(-1, 1) == -1
-    assert divide(5, 2) == 2.5
+    assert divide(6, 3) == 2, 'The input results do not match the output'
+    assert divide(-1, 1) == -1, 'The input results do not match the output'
+    assert divide(5, 2) == 2.5, 'The input results do not match the output'
 
 def test_divide_by_zero():
     try:
